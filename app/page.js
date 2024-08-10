ffe7060614e45e23446d5e2ef8732b5df01deb9b
mileshall1 <156925206+mileshall1@users.noreply.github.com>
2024-08-10 13:58:47 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,31 @@
+'use client'
 import Image from "next/image";
 import { Box } from "@mui/material";
+import {useState) from 'react'
 
 export default function Home() {
-  return <Box>Hello Lebron</Box>;
+  const [messages, setMessages] = useState({
+    role: 'assistant',
+    content: 'Hi I am Lebron James, how can I assist you today?'
+  })
+
+    const [message, setMessage] = useState('')
+  return 
+    <Box 
+      width = "100vw"
+        height = "100vh"
+          display = "flex"
+            flexDirection = "column"
+              justifyContent = "center"
+                alignItems = "center"
+                  >
+                  <Stack 
+                  direction = "column"
+                    width = "600px"
+                      height = "700px"
+                        border = "1px solid black"
+                          p={2}
+                            spacing={2}
+                        
+    </Box>;
 }
