23f3a1f2a12cbdd42ff667a6796901a2abe965bd
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-08-12 23:44:05 -0700

    Add Lebron James photo to the left of chat

--- app/page.js
+++ app/page.js
@@ -91,69 +91,95 @@ export default function Home() {
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
-        // bgcolor={LakersTheme.palette.primary.light}
         sx={{ background: LakersTheme.palette.gradients.primary }}
       >
-        <Stack
-          direction="column"
-          width="600px"
-          height="600px"
-          // border="1px solid black"
-          p={2}
-          spacing={2}
-        >
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Box
+            sx={{
+              height: 633,
+              width: 400,
+              marginRight: 2,
+              backgroundSize: "400px",
+              backgroundRepeat: "no-repeat",
+              backgroundImage: "url(lebron-poster-dunk.png)",
+            }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          />
           <Stack
             direction="column"
+            width="600px"
+            height="600px"
+            p={2}
             spacing={2}
-            flexGrow={1}
-            overflow="auto"
-            maxHeight="100%"
           >
-            {messages.map((message, index) => (
-              <Box
-                key={index}
-                display="flex"
-                justifyContent={
-                  message.role === "assistant" ? "flex-start" : "flex-end"
-                }
-              >
-                <Box
-                  bgcolor={
-                    message.role === "assistant"
-                      ? "primary.main"
-                      : "secondary.main"
-                  }
-                  color={message.role === "assistant" ? "white" : "black"}
-                  borderRadius={16}
-                  p={3}
-                >
-                  {message.content}
-                </Box>
-              </Box>
-            ))}
-          </Stack>
-          <Stack direction="row" spacing={2}>
-            <TextField
-              label="Message"
-              fullWidth
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <Button
-              variant="contained"
-              onClick={sendMessage}
-              sx={{
-                backgroundColor: "primary.main",
-                "&:hover": {
-                  backgroundColor: "secondary.main",
-                  color: "black",
-                },
-              }}
+            <Stack
+              direction="column"
+              spacing={2}
+              flexGrow={1}
+              overflow="auto"
+              maxHeight="100%"
             >
-              Send
-            </Button>
+              {messages.map((message, index) => (
+                <Box
+                  key={index}
+                  display="flex"
+                  justifyContent={
+                    message.role === "assistant" ? "flex-start" : "flex-end"
+                  }
+                >
+                  <Box
+                    bgcolor={
+                      message.role === "assistant"
+                        ? "primary.main"
+                        : "secondary.main"
+                    }
+                    color={message.role === "assistant" ? "white" : "black"}
+                    borderRadius={16}
+                    p={3}
+                  >
+                    {message.content}
+                  </Box>
+                </Box>
+              ))}
+            </Stack>
+            <Stack direction="row" spacing={2}>
+              <TextField
+                label="Message"
+                fullWidth
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <Button
+                variant="contained"
+                onClick={sendMessage}
+                sx={{
+                  backgroundColor: "primary.main",
+                  "&:hover": {
+                    backgroundColor: "secondary.main",
+                    color: "black",
+                  },
+                }}
+              >
+                Send
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
+
+          {/* Image on the right */}
+          <Box
+            component="img"
+            sx={{
+              height: 633,
+              width: 400,
+              borderRadius: "50%",
+              marginLeft: 2,
+            }}
+            alt="Right Avatar"
+            src="https://your-image-url.com/right-avatar.jpg"
+          />
+        </Box>
       </Box>
     </ThemeProvider>
   );
